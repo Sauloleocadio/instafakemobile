39f1c51dbdca43a11032e58b171a7f2307dc825d
Saulo leocadio <saulomanshu@gmail.com>
2019-09-22 22:31:52 -0300

    Arquivos do instafake parte mobile

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { createAppContainer, createStackNavigator } from "react-navigation";
+
+import Feed from "./pages/Feed";
+import New from "./pages/New";
+
+export default createAppContainer(
+  createStackNavigator(
+    {
+      Feed,
+      New
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitle: "Taynara Linda maravilhosa "
+      }
+    }
+  )
+);
